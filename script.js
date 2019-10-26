8b738676e9ad35428b8e7c3572220704db1265f8
Alex-Edward_Klim <alexklim3221128@gmail.com>
2019-10-26 21:14:53 +0300

    feat: implement js drawing matrixes and picture

--- script.js
+++ script.js
@@ -16,3 +16,47 @@ for (let i = 0; i < sizeOptionsButtons.length; i++) {
     sizeOptionsButtonImages[i].src = './assets/images/Ellipse2.svg';
   });
 }
+
+
+
+document.getElementById('4x4').addEventListener('click', function() {
+  let colorMatrix;
+  let url = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json';
+  fetch(url)
+  .then(res => res.json())
+  .then((out) => {
+    colorMatrix = out;
+    for (let i = 0; i < colorMatrix.length; i++) {
+      for (let j = 0; j < colorMatrix[i].length; j++) {
+        ctx.fillStyle = `#${colorMatrix[i][j]}`;
+        ctx.fillRect(i * 128, j * 128, (i + 1) * 128, (j + 1) * 128);
+      }
+    }
+  })
+  .catch(err => { throw err });
+});
+
+document.getElementById('32x32').addEventListener('click', function() {
+  let colorMatrix;
+  let url = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json';
+  fetch(url)
+  .then(res => res.json())
+  .then((out) => {
+    colorMatrix = out;
+    for (let i = 0; i < colorMatrix.length; i++) {
+      for (let j = 0; j < colorMatrix[i].length; j++) {
+        ctx.fillStyle = `rgba(${colorMatrix[i][j][0]}, ${colorMatrix[i][j][1]}, ${colorMatrix[i][j][2]}, ${colorMatrix[i][j][3]})`;
+        ctx.fillRect(i * 16, j * 16, (i + 1) * 16, (j + 1) * 16);
+      }
+    }
+  })
+  .catch(err => { throw err });
+});
+
+document.getElementById('256x256').addEventListener('click', function() {
+  let image = new Image();
+  image.src = "./data/image.png";
+  image.onload = function() {
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+  }
+});
